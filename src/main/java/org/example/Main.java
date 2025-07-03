5a6a075bba340a5ff4838659fb51393ffcb6c697
Justin16Chen <justin.chen0160@gmail.com>
2025-07-03 11:31:09 -0400

    added djl files in build.gradle

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import org.example.patternControlPanel.SceneManager.SceneManager;
 
-public class Main extends Application{
+public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
