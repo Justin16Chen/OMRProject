02c4c022dfd9e890d948a7dc1b5c6f46831d52cc
Justin16Chen <justin.chen0160@gmail.com>
2025-07-05 12:22:31 -0400

    added testing code to run python in java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,10 @@ import javafx.stage.Stage;
 import org.example.patternControlPanel.sceneManager.CustomApplication;
 import org.example.patternControlPanel.sceneManager.SceneManager;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/org/example/RunPythonInJava.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RunPythonInJava {
+    public static void main(String[] args) {
+        String pythonPath = "C:\\Users\\justi\\anaconda3\\envs\\omrEnv\\python.exe";
+        String scriptPath = "python\\test.py";
+
+        ProcessBuilder pb = new ProcessBuilder(pythonPath, scriptPath);
+
+        try {
+            Process process = pb.start();
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream())
+            );
+            String line;
+            while((line = reader.readLine()) != null)
+                System.out.println(line);
+
+            BufferedReader errorReader = new BufferedReader(
+                    new InputStreamReader(process.getErrorStream())
+            );
+            while((line = errorReader.readLine()) != null)
+                System.err.println("Error: " + line);
+
+            int exitCode = process.waitFor();
+            System.out.println("Exited with code: " + exitCode);
+        }
+        catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
